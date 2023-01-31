15a09e74efaebeb5903b2bae62cff86a68d972b3
Ravi Kr Sharma <ravirockskkr@gmail.com>
2023-01-31 12:24:57 +0530

    initial commit

---
+++ components/Tutorial.vue
@@ -0,0 +1,12 @@
+<!-- Please remove this file from your project -->
+<template>
+ <div>
+  <h1><i>This Is Sample Project For CloudFlare Purpose</i></h1>
+ </div>
+</template>
+
+<script>
+export default {
+  name: 'NuxtTutorial'
+}
+</script>
